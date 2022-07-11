c09f7bcf99cb0d9454aa56019975d008753c0450
nghiaha1 <maxx.minimizee@gmail.com>
2022-07-11 21:07:39 +0700

    update

--- src/main/java/spring/springassignment/api/AccountApi.java
+++ src/main/java/spring/springassignment/api/AccountApi.java
@@ -4,15 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import spring.springassignment.entity.Account;
 import spring.springassignment.repository.AccountRepository;
 
 @RestController
 @RequestMapping(path = "api/v1/accounts")
+@CrossOrigin("*")
 public class AccountApi {
     @Autowired
     PasswordEncoder passwordEncoder;
